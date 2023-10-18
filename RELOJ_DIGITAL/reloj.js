56e0a6303a99a4ed1d08e52dd1f45a078db4e3e6
ThomasCastro2005 <118085112+ThomasCastro2005@users.noreply.github.com>
2023-10-18 13:26:46 -0500

    RELOJ  agregado

---
+++ RELOJ_DIGITAL/reloj.js
@@ -0,0 +1,57 @@
+(function() {
+    var actualizarHora = function() {
+        var fecha = new Date(),
+            hora = fecha.getHours(),
+            ampm,
+            minutos = fecha.getMinutes(),
+            segundos = fecha.getSeconds(),
+            diaSemana = fecha.getDay(),
+            dia = fecha.getDate(),
+            mes = fecha.getMonth(),
+            year = fecha.getFullYear(); 
+
+        var pHoras = document.getElementById('horas'),
+            pAMPM = document.getElementById('ampm'),
+            pMinutos = document.getElementById('minutos'),
+            pSegundos = document.getElementById('segundos'),
+            pDiaSemana = document.getElementById('diaSemana'),
+            pDia = document.getElementById('dia'),
+            pMes = document.getElementById('mes'),
+            pYear = document.getElementById('year'); 
+
+        var semana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+        pDiaSemana.textContent = semana[diaSemana];
+        pDia.textContent = dia;
+        var meses = ['Enero', 'Febrero','Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+        pMes.textContent = meses[mes];
+        pYear.textContent = year; 
+
+        if (hora >= 12 ){
+            hora = hora-12;
+            ampm = 'PM';
+        }else{
+            ampm = 'AM';
+        }
+
+        if (hora == 0){
+            hora =12;
+        }
+
+        
+        pHoras.textContent = hora;
+        pAMPM.textContent = ampm;
+
+        if (minutos < 10){
+            minutos = "0" + minutos;
+        }
+        if (segundos < 10){
+            segundos = "0"+ segundos;
+        }
+        pMinutos.textContent = minutos;
+        pSegundos.textContent = segundos;
+        
+    };
+
+    actualizarHora();
+    var intervalo = setInterval(actualizarHora, 1000)
+})();
